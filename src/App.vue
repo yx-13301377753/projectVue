709323b820ca5c78be940cb2caff9574f35de27a
yx-13301377753 <57569099+yx-13301377753@users.noreply.github.com>
2019-12-04 10:56:57 +0800

    2019/12/4

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  <v-header />
+  </div>
+</template>
+
+<script>
+import VHeader from '@/components/header/header.vue';
+
+export default {
+  components: {
+    VHeader
+  }
+}
+</script>
+
+
